818f8b5115eff135d523dde0663399ac19b4e56f
Fatih Selim Yakar <32717115+fatihselimyakar@users.noreply.github.com>
2020-01-21 15:25:58 +0300

    Updated Git Clone Command

--- Group7-Test/deneme.py
+++ Group7-Test/deneme.py
@@ -12,31 +12,43 @@ git_id = data["github_login"]
 git_pass = data["github_password"]
 target_pass = data["targetPswd"]
 addressClone = address.replace("https://","https://"+git_id+":"+git_pass+"@")
+#New attribute
+buildFolder = data["forBuild"]
+projectName = data["project_name"]
 
-
-projectName = subprocess.check_output(['basename', addressClone, ".git"])
-projectName = projectName[:-1]
-
+#Clone dilen directory name bulmak icin yapiyoruz
+cloneDirectoryName = subprocess.check_output(['basename', addressClone, ".git"])
+cloneDirectoryName = cloneDirectoryName[:-1]
+#Bize verilecek örnek repo adresi (direk pom.xml içinde)
+#https://github.com/canerkarakas/GtuDevOps
+#Updating project dir
+#Ornek icin asagidaki concat islemi su path'i verir: GtuDev/Deneme12/SumNumbers-master
+projectFolder = cloneDirectoryName+"/"+projectName+"/"+buildFolder
 def start_testing():
     print("Project Name: "+projectName)
     print("Cloning Repo...")
     os.system("pwd")
-    os.system("git clone "+addressClone)
+    os.system("git clone "+addressClone+ " osman")
     filePath = subprocess.check_output(["pwd"])
     filePath = filePath[:-1]
-    os.chdir(filePath+"/"+projectName)
+    #Project ve pom.xml olan dizine giriliyor
+    os.chdir(projectFolder)
     print("Cloned into:")
     os.system("pwd")
     print("Maven Test Starting...")
+    #Test basliyor
     os.system("mvn test")
     path = os.getcwd()
     os.chdir(os.path.abspath(os.path.join(path, os.pardir)))
 
 def parse_result():
-    path = projectName+"/target/surefire-reports"
-    for filename in os.listdir(path):
+    filePath = subprocess.check_output(["pwd"])
+    filePath = filePath[:-1]
+    #Tekrardan report olan folder'a giriliyor. Eger hata varsa print ile dogru path'i ekleyin.
+    filePath = filePath+"/"+buildFolder+"/target/surefire-reports"
+    for filename in os.listdir(filePath):
         if not filename.endswith('.xml'): continue
-        fullname = os.path.join(path, filename)
+        fullname = os.path.join(filePath, filename)
         tree = ET.parse(fullname)
     root = tree.getroot()
     return root
@@ -51,6 +63,8 @@ def create_json():
         data["git_pass"] = git_pass
         data["testresult"] = "fail"
         data["deployment"] = "nodeploy"
+        data["forBuild"] = buildFolder
+        data["projectName"] = projectName
         data["targetPswd"] = target_pass
         data["repoUrl"] = address
         data["failures"] = int(parse_result().attrib["failures"])
@@ -75,6 +89,8 @@ def create_json():
         data["git_pass"] = git_pass
         data["testresult"] = "success"
         data["deployment"] = "deploy"
+        data["forBuild"] = buildFolder
+        data["projectName"] = projectName
         data["targetPswd"] = target_pass
         data["repoUrl"] = address
         data["failures"] = int(parse_result().attrib["failures"])
@@ -95,11 +111,13 @@ def create_json():
         print(json_data)
 def delete_test_env():
     print("Test environment deleting...")
-    if os.path.exists(projectName) and os.path.isdir(projectName):
-        shutil.rmtree(projectName)
+    os.chdir("..")
+    filePath = subprocess.check_output(["pwd"])
+    filePath = filePath[:-1]
+    if os.path.exists(filePath) and os.path.isdir(filePath):
+        shutil.rmtree(filePath)
 
 start_testing()
 parse_result()
 create_json()
 delete_test_env()
-
